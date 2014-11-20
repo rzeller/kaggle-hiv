c086a8946392d65bf12f00ea256b350785d45618
rzeller <zeller@factor-14.com>
2014-11-19 17:09:51 -0800

    Worked toward ambiguous translation

--- LoadData.py
+++ LoadData.py
@@ -13,7 +13,7 @@ for d in data:
 
 
 
-codon={'TTT':'F','TTC':'F',
+codonDict={'TTT':'F','TTC':'F',
 	'TTA':'L','TTG':'L','CTT':'L','CTC':'L','CTA':'L','CTG':'L',
 	'ATT':'I','ATC':'I','ATA':'I',
 	'ATG':'M',
@@ -23,7 +23,7 @@ codon={'TTT':'F','TTC':'F',
 	'ACT':'T','ACC':'T','ACA':'T','ACG':'T',
 	'GCT':'A','GCC':'A','GCA':'A','GCG':'A',
 	'TAT':'Y','TAC':'Y',
-	'TAA':'X','TAG':'X',
+	'TAA':'*','TAG':'*',
 	'CAT':'H','CAC':'H',
 	'CAA':'Q','CAG':'Q',
 	'AAT':'N','AAC':'N',
@@ -31,40 +31,70 @@ codon={'TTT':'F','TTC':'F',
 	'GAT':'D','GAC':'D',
 	'GAA':'E','GAG':'E',
 	'TGT':'C','TGC':'C',
-	'TGA':'X',
+	'TGA':'*',
 	'TGG':'W',
 	'CGT':'R','CGC':'R','CGA':'R','CGG':'R',
 	'AGT':'S','AGC':'S',
 	'AGA':'R','AGG':'R',
-	'GGT':'G','GGC':'G','GGA':'G','GGG':'G',
-	'GCN':'A',
-	'CGN':'R','MGR':'R',
-	'AAY':'N',
-	'GAY':'D',
-	'TGY':'C',
-	'CAR':'Q',
-	'GAR':'E',
-	'GGN':'G',
-	'CAY':'H',
-	'ATH':'I',
-	'YTR':'L','CTN':'L',
-	'AAR':'K',
-	'TTY':'F',
-	'CCN':'P',
-	'TCN':'S','AGY':'S',
-	'ACN':'T',
-	'TAY':'Y',
-	'GTN':'V',
-	'TAR':'X','TRA':'X',
-	'YTA':'L',
-	'GCW':'A',
-	'CGM':'R',
-	'CCM':'P',
-	'TTR':'L',
-	'KCA':'-',
-	'ARG':'-',
-	'WTA':'-',
-	'YAT':'-'}
+	'GGT':'G','GGC':'G','GGA':'G','GGG':'G'}
+	# 'GCN':'A',
+	# 'CGN':'R','MGR':'R',
+	# 'AAY':'N',
+	# 'GAY':'D',
+	# 'TGY':'C',
+	# 'CAR':'Q',
+	# 'GAR':'E',
+	# 'GGN':'G',
+	# 'CAY':'H',
+	# 'ATH':'I',
+	# 'YTR':'L','CTN':'L',
+	# 'AAR':'K',
+	# 'TTY':'F',
+	# 'CCN':'P',
+	# 'TCN':'S','AGY':'S',
+	# 'ACN':'T',
+	# 'TAY':'Y',
+	# 'GTN':'V',
+	# 'TAR':'X','TRA':'X',
+	# 'YTA':'L',
+	# 'GCW':'A',
+	# 'CGM':'R',
+	# 'CCM':'P',
+	# 'TTR':'L',
+	# 'KCA':'-',
+	# 'ARG':'-',
+	# 'WTA':'-',
+	# 'YAT':'-'}
+
+ambiguity = {'K':['G','T'],
+	'M':['A','C'],
+	'R':['A','G'],
+	'Y':['C','T'],
+	'S':['C','G'],
+	'W':['A','T'],
+	'B':['C','G','T'],
+	'V':['A','C','G'],
+	'H':['A','C','T'],
+	'D':['A','G','T'],
+	'N':['G','A','C','T']}
+
+def translate(nucStr,codonDict,ambiguity):
+	aaStr=''
+	for ic in range(len(pro)/3):
+		codon=pro[3*j:3*(j+1)]
+		if codon in codonDict:
+			aaStr=codonDict[codon]
+		else:
+			#for each ambiguous nucleotide
+			#create new codon possibilities
+			#if all codon possiblities are the same, it's that aa
+			#else, no idea
+
+
+
+
+
+
 
 
 aas=[]
