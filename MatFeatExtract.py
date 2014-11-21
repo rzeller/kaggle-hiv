57a7522380db9761e739fc9ac5a8c1745ce099f8
rzeller <zeller@factor-14.com>
2014-11-20 17:52:34 -0800

    implemented average blocks

--- MatFeatExtract.py
+++ MatFeatExtract.py
@@ -2,20 +2,14 @@ import numpy as np
 
 def PseudoPSSM(M):
 	#Pseudo-PSSM code as described in Nanni, Lumini, and Brahnam (2014)
-
-	###Input###
+	###Inputs###
     # M: Nx20 array (matrix) to extract features from
-
-    ###Output###
+    ###Outputs###
     # PP: 1-D feature array with 320 values
 
     N = np.shape(M)[0]
-
     E = (M - np.mean(M,1).reshape(N,1))/np.std(M,1).reshape(N,1)
-    
-
     PP = np.zeros((320,1))
-
     PP[:20,0] = np.mean(E,0)
     print PP
 
@@ -26,7 +20,17 @@ def PseudoPSSM(M):
 
 
 
-# def AverageBlocks(M):
+def AverageBlocks(M):
+	#Average Blocks as described in Nanni, Lumini, and Brahnam (2014)
+	###Inputs###
+    # M: Nx20 array (matrix) to extract features from
+    ###Outputs###
+    # AB: 1-D feature array with 400 values
+	N=np.shape(M)[0]
+	AB=np.zeros((20,20))
+	for i in range(20):
+		AB[i,:]=np.mean(M[round(i*N/20.):min(N,round((i+1)*N/20.)),:],0)
+	return AB.reshape(400,1)
 
 # def SVD(M):
 
@@ -38,7 +42,7 @@ def PseudoPSSM(M):
 
 
 
-### Test PseudoPSSM
 M=np.random.randn(100,20)
-PP=PseudoPSSM(M)
-print PP
+# featvec=PseudoPSSM(M)
+featvec=AverageBlocks(M)
+print featvec
