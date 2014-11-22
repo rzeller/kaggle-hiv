7a2578636318c533330aaa2d853a674d05ccb875
rzeller <zeller@factor-14.com>
2014-11-21 17:39:01 -0800

    implemented matrix representation and feature extraction

--- Train.py
+++ Train.py
@@ -4,7 +4,7 @@ import MatrixRep
 from MatFeatExtract import *
 
 
-database='/Users/Robby/Documents/Factor14/Sequence-Databases/databases/uniprot_sport.fasta'
+database='/Users/Robby/Documents/Factor14/Sequence-Databases/databases/uniprot_sprot.fasta'
 
 data=[]
 with open('training_data.csv', 'rb') as csvfile:
@@ -14,18 +14,24 @@ with open('training_data.csv', 'rb') as csvfile:
 		data.append((int(row[0]),int(row[1]),row[2],row[3],float(row[4]),int(row[5])))
 
 #Run through data, create representations, extract features, and aggregate
-for d in data[:1]:
+for d in data[:3]:
 	#Load and translate data
 	protease=Seq(d[3])
 	proaas=protease.translate()
 	
 	#Create a representation of the protein
 	output=MatrixRep.PSSM(proaas.tostring(),database=database)
-	mat=output['PSSM Values']
+	mat=output['PSSM Values'].astype(float)
 	
 	## Run a feature extraction method on mat
+	feature=PseudoPSSM(mat)
 
 	## Aggregate Features
+	if 'features' not in locals():
+		features=feature.copy()
+	else:
+		features=np.vstack((features,feature))
+
 
 
 #Do Machine Learning
